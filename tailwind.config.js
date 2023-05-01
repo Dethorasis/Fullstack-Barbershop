8b20f25e5f91ccc3c323843f67530203db923098
cade fitchett <rscador123@gmail.com>
2023-05-01 14:24:21 +1200

    set up and installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./client/**/*.{html,js,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        pacifico: ['"Pacifico"', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
