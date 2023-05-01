d1685ffe27ebb653b563a8c242de1de9a880c407
cade fitchett <rscador123@gmail.com>
2023-05-01 13:17:37 +1200

    getServices client route created

--- client/apis/services.ts
+++ client/apis/services.ts
@@ -1,1 +1,9 @@
+import request from 'superagent'
+import { Services } from '../../models/Services'
 
+const serviceURL = '/'
+
+export async function getServices() {
+  const response = await request.get(serviceURL)
+  return response.body as Services[]
+}
