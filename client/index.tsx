8473ac3b6afcc11677d33e9d8be147ac09dc0a2e
cade fitchett <rscador123@gmail.com>
2023-05-01 13:29:39 +1200

    wrapped app in Router

--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
-  createRoot(document.getElementById('app') as HTMLElement).render(<App />)
+  createRoot(document.getElementById('app') as HTMLElement).render(
+    <Router>
+      <App />
+    </Router>
+  )
 })
