87f3e4946771590f99149a5d30d3e674cd7840d0
Cade Fitchett <cadefitchett1@gmail.com>
2023-08-22 15:27:05 +1200

    can now refresh pages without app breaking

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import { join } from 'node:path'
-
 import salonRoutes from './routes/salon'
 
 const server = express()
@@ -8,6 +7,12 @@ const server = express()
 server.use(express.json())
 server.use(express.static(join(__dirname, 'public')))
 
+// Add your API routes here
 server.use('/api/v1/serviceroutes', salonRoutes)
+
+// Serve the main HTML file for all routes
+server.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'public', 'index.html'))
+})
 
 export default server
