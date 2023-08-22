c070327d7bd855ad8c6b3ddcb103ff00577f246a
cade fitchett <rscador123@gmail.com>
2023-08-23 10:39:04 +1200

    created basic seed data and migration for contact page

---
+++ server/db/migrations/20230730042203_contact.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('contact', function (table) {
+    table.increments('id')
+    table.string('url')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('contact')
+}
