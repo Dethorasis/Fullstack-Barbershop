2d3d893a240a89edf11c773f4cb45da38e629311
Cade Fitchett <cadefitchett1@gmail.com>
2023-08-22 15:16:36 +1200

    changed serviceURL to correct path

--- client/apis/services.ts
+++ client/apis/services.ts
@@ -1,7 +1,7 @@
 import request from 'superagent'
 import { Services } from '../../models/Services'
 
-const serviceURL = '/'
+const serviceURL = '/api/v1/serviceroutes'
 
 export async function getServices() {
   const response = await request.get(serviceURL)
