44bda83e4f49c7053baa25232d94ac5ba7fdebb7
Cade Fitchett <cadefitchett1@gmail.com>
2023-08-26 11:07:39 +1200

    added get gallery function to db

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,6 +1,11 @@
 import connection from './connection'
 import type { ServiceModel } from '../../models/Services'
+import { GalleryModel } from '../../models/Gallery'
 
 export function getServices(db = connection): Promise<ServiceModel[]> {
   return db('services').select()
+}
+
+export function getGallery(db = connection): Promise<GalleryModel[]> {
+  return db('gallery').select()
 }
