56260ea14ebaeb85ee93fbddbae6f8d23531d084
Cade Fitchett <cadefitchett1@gmail.com>
2023-08-26 12:03:42 +1200

    added gallery route + added gallery to server file

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { join } from 'node:path'
-import salonRoutes from './routes/salon'
+import salon from './routes/salon'
+import gallery from './routes/gallery'
 
 const server = express()
 
@@ -8,7 +9,8 @@ server.use(express.json())
 server.use(express.static(join(__dirname, 'public')))
 
 // Add your API routes here
-server.use('/api/v1/serviceroutes', salonRoutes)
+server.use('/api/v1/serviceroutes', salon)
+server.use('/api/v1/galleryroutes', gallery)
 
 // Serve the main HTML file for all routes
 server.get('*', (req, res) => {
