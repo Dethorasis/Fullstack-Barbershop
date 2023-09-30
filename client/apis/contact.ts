c316864b8cef442d8bc902e390d1ac4456d7b46c
Cade Fitchett <cadefitchett1@gmail.com>
2023-09-30 14:39:48 +1300

    added api to update contact

--- client/apis/contact.ts
+++ client/apis/contact.ts
@@ -1,8 +1,22 @@
 import request from 'superagent'
+import { ContactModel } from '../../models/Contact'
 
 const contactURL = '/api/v1/contactroutes'
 
 export async function getContact() {
   const response = await request.get(contactURL)
   return response.body
+}
+
+export async function updateServices(updatedContact: ContactModel) {
+  try {
+    const response = await request.put(contactURL).send(updatedContact)
+    if (response.status === 200) {
+      return response.body
+    } else {
+      throw new Error('Failed to update contact')
+    }
+  } catch (error) {
+    console.error('Error updating contact:')
+  }
 }
