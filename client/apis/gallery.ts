37a03c582f0873b39f5615dc929e8ff86fb8d948
Cade Fitchett <cadefitchett1@gmail.com>
2023-10-12 16:22:41 +1300

    Admin can now upload images

--- client/apis/gallery.ts
+++ client/apis/gallery.ts
@@ -1,8 +1,22 @@
 import request from 'superagent'
+import { GalleryModel } from '../../models/Gallery'
 
 const galleryURL = '/api/v1/galleryroutes'
 
 export async function getGallery() {
   const response = await request.get(galleryURL)
   return response.body
+}
+
+export async function addGalleryImage(gallery: GalleryModel) {
+  try {
+    const response = await request.post(galleryURL).send(gallery)
+    if (response.status === 200) {
+      return response.body
+    } else {
+      throw new Error('Failed to add image')
+    }
+  } catch (error) {
+    console.error('Error adding image:')
+  }
 }
