864d4ad73c0ce61deaab1c42b5b53352f9e4e1d3
Cade Fitchett <cadefitchett1@gmail.com>
2023-11-11 13:01:06 +1300

    Now conditionally renders login page if authentication is false

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,13 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Home from './Home'
 import Nav from './Nav'
 import ServicesPage from './Services'
 import GalleryPage from './Gallery'
 import ContactPage from './Contact'
-// import AddServices from './admin/AddServices'
 import AdminServices from './admin/Services/AdminServices'
 import AdminContact from './admin/Contact/AdminContact'
 import AdminGallery from './admin/Gallery/AdminGallery'
+import Login from './Login'
+
+const isAuthenticated = () => {
+  return true // TODO: Add logic to determine authentication status here
+}
 
 function App() {
   return (
@@ -19,10 +23,26 @@ function App() {
           <Route path="/services" element={<ServicesPage />} />
           <Route path="/gallery" element={<GalleryPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/login" element={<Login />} />
 
-          <Route path="/admin/services" element={<AdminServices />} />
-          <Route path="/admin/contact" element={<AdminContact />} />
-          <Route path="/admin/gallery" element={<AdminGallery />} />
+          <Route
+            path="/admin/services"
+            element={
+              isAuthenticated() ? <AdminServices /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/admin/contact"
+            element={
+              isAuthenticated() ? <AdminContact /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/admin/gallery"
+            element={
+              isAuthenticated() ? <AdminGallery /> : <Navigate to="/login" />
+            }
+          />
         </Routes>
       </div>
     </>
